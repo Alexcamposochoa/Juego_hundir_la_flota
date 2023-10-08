35a0d4a5a9e79e7c3d7d2f46ca6fb0205c7507d2
protonaco <145392912+protonaco@users.noreply.github.com>
2023-10-08 19:58:15 +0200

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,12 @@
+from funciones import *
+from funciones import colocar_barcos
+from funciones import disparo_jugador
+from funciones import turno_maquina
+from funciones import bienvenida
+from funciones import crear_tablero
+
+tablero = crear_tablero()
+tablero_maquina = crear_tablero()
+tablero_adivinar = crear_tablero()
+vidas_jugador = 20
+vidas_maquina = 5
