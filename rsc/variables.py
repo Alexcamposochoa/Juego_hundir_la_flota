e38a7a17538c62284d6b8d4df3dc7e2cabbc6337
protonaco <alex.campos.ochoa@gmail.com>
2024-01-23 15:01:46 +0100

    update

---
+++ rsc/variables.py
@@ -0,0 +1,7 @@
+import numpy as np
+
+tablero = tablero = np.full((10, 10), ' ')
+tablero_maquina = np.full((10, 10), ' ')
+tablero_adivinar = np.full((10, 10), ' ')
+vidas_jugador = 20
+vidas_maquina = 5
